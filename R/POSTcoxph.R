71ef358fa5673b2eef7bd9cafe9c831b657e98fc
xueyuancao <xueyuan.cao@stjude.org>
2016-11-02 10:47:16 -0500

    Revision Nov2

--- R/POSTcoxph.R
+++ R/POSTcoxph.R
@@ -2,8 +2,8 @@ POSTcoxph<-function(exprSet,
                   geneSet,                         
                   lamda=0.95,                      
                   nboots=100,                      
-                  modd='Surv(EFSTIME, EFSCENSOR) ~ strata(arm2)',  
-                  seed=13)                                                                                         
+                  model='Surv(EFSTIME, EFSCENSOR) ~ strata(arm2)',  
+                  seed=13,...)                                                                                         
 {
   sttime<-Sys.time()
   str.opt<-options()$stringsAsFactors
@@ -16,62 +16,58 @@ POSTcoxph<-function(exprSet,
   expr<-exprs(exprSet)
   clin<-pData(exprSet)
   
-  vars<-modd
-  splc<-c("~", ",",  "+", "(", ")")
-  for (i in 1:length(splc))
-  {
-    vars<-unlist(strsplit(vars, splc[i], fixed =TRUE)) 
-  }
-  vars<-gsub(" ", "", vars, fixed=TRUE)
-  vars<-vars[!is.element(toupper(vars), c("","SURV", "FACTOR","AS.FACTOR","STRATA"))]
-  
+  spec.str<-c("SURV", "FACTOR","AS.FACTOR","STRATA")
+  vars<-unlist(strsplit(model, "[,() ~]+"))
+  vars<-vars[!is.element(toupper(vars), spec.str)] 
   if (sum(!is.element(vars, dimnames(clin)[[2]]))!=0)
-  { print("Model Specification Error:"); break()} 
-  var2<-unlist(strsplit(modd, "~"))
+  { print("Model Specification Error:"); break()}
+   
+  var2<-unlist(strsplit(model, "~"))
   var2<-gsub(" ", "", var2, fixed=TRUE)
-  var2<-var2[!is.element(var2, '')]
-  if ( length(var2)==2 ) newmod<-paste(var2[1], "~ prb + ", var2[2], sep="")
-  if ( length(var2)==1 ) newmod<-paste(var2[1], "~ prb", sep="")
+  if (var2[2]!="" ) newmodel<-paste(var2[1], "~ prb + ", var2[2], sep="")
+  if (var2[2]=="" ) newmodel<-paste(var2[1], "~ prb", sep="")
   nsub<-nrow(clin)
   res<-NULL
   
   set.seed(seed)
   perms<-matrix(NA, nboots, nsub)
-  for (i in 1:nboots) perms[i,]<-sample(nsub,replace=TRUE)
-    
-  for (i in 1:length(geneSet)){
+  for (i in seq_len(nboots)) perms[i,]<-sample(nsub,replace=TRUE)
+  
+  nSet<-length(geneSet)  
+  for (i in seq_len(nSet))
+  {
     thisSet<-geneSet[[i]]
-    this<-geneIds(thisSet)
-    this.pot<-expr[is.element(dimnames(expr)[[1]], this), ]
+    thisIds<-geneIds(thisSet)
+    this.pot<-expr[is.element(dimnames(expr)[[1]], thisIds), ]
     if (nrow(this.pot)>1)
     {
        egd<-eigen(cov(t(this.pot)))
        val<-egd$values
        vec<-egd$vectors
        if (is.null(lamda)) nf<-length(val)
-          else if (is.numeric(lamda)) 
-                   nf<-min(c(1:length(val))[cumsum(val)/sum(val) >=lamda])
+       if (is.numeric(lamda)) 
+           nf<-min(seq_len(length(val))[cumsum(val)/sum(val) >=lamda])
        if (nf >1)
        {          
-         AA<-diag(val[1:nf])
-         prj.pot<-t(t(this.pot)%*%vec[, 1:nf])
+         AA<-diag(val[seq_len(nf)])
+         prj.pot<-t(t(this.pot)%*%vec[, seq_len(nf)])
          zstat<-apply(prj.pot, 1, function(prb, mdat){
                 tdd<-cbind.data.frame(prb, mdat)
-                tt<-coxph(as.formula(newmod), data=tdd)
+                tt<-coxph(as.formula(newmodel), data=tdd ,...)
                 return(tt$coefficients[1]/sqrt(diag(tt$var)[1]))
                 }, clin[, vars])
          qstat<-t(zstat)%*%AA%*%zstat
              
-         boots<-NULL
-         for (j in 1:nboots)
+         boots<-matrix(NA, nboots, nf)
+         for (j in seq_len(nboots))
          {
             this.boot<-perms[j,]
-            boot.t<-apply(prj.pot, 1, function(prb, mdat){
+            boot.res<-apply(prj.pot, 1, function(prb, mdat){
                 tdd<-cbind.data.frame(prb, mdat)
-                tt<-coxph(as.formula(newmod), data=tdd)
+                tt<-coxph(as.formula(newmodel), data=tdd,...)
                 return(tt$coefficients[1]/sqrt(diag(tt$var)[1]))
                 }, clin[this.boot, vars])
-          boots<-rbind(boots, boot.t)
+            boots[j, ]<-boot.res
          }
          sigma<-cor(boots)
          Asigma<-AA%*%sigma
@@ -86,25 +82,23 @@ POSTcoxph<-function(exprSet,
       if (nf ==1)
       {
         tdd<-cbind.data.frame(prb=t(this.pot)%*%vec[, 1], clin[, vars])
-        tt<-coxph(as.formula(newmod), data=tdd)
+        tt<-coxph(as.formula(newmodel), data=tdd ,...)
         zstat<-tt$coefficients[1]/sqrt(diag(tt$var)[1])
         pval<-pchisq(zstat, 1, lower.tail=FALSE)
         this.res<-cbind(Gene=setName(thisSet),Nprobe=nrow(this.pot), 
                         Ndim=1,  Stat=zstat, Pvalue=pval)
         res<-rbind(res, this.res) 
-        #print(paste(i, nf, sep=": "))
       }    
     }    
     if ( nrow(this.pot)==1)
     {
         tdd<-cbind.data.frame(prb=as.numeric(this.pot), clin[, vars])
-        tt<-coxph(as.formula(newmod), data=tdd)
+        tt<-coxph(as.formula(newmodel), data=tdd,...)
         zstat<-tt$coefficients[1]/sqrt(diag(tt$var)[1])
         pval<-pchisq(zstat, 1, lower.tail=FALSE)
         this.res<-cbind(Gene=setName(thisSet),Nprobe=nrow(this.pot), 
                         Ndim=1,  Stat=zstat, Pvalue=pval)
         res<-rbind(res, this.res)
-        #print(paste(i, 1, sep=": "))
     }
   } 
   dimnames(res)[[2]]<-c("GeneSet", "Nprobe", "Nproj", 'Stat', 'p.value')
--- R/POSTglm.R
+++ R/POSTglm.R
@@ -3,7 +3,7 @@ POSTglm<-function(exprSet,
                   lamda=0.95,                      
                   seed=13,                         
                   nboots=100,                      
-                  modd='Group ~ ',                 
+                  model='Group ~ ',                 
                   family=binomial(link = "logit"), 
                   ...)                                                    
 {
@@ -16,32 +16,27 @@ POSTglm<-function(exprSet,
   expr<-exprs(exprSet)
   clin<-pData(exprSet)
   
-  vars<-modd
-  splc<-c("~", "+", "(", ")")
-  for (i in 1:length(splc))
-  {
-    vars<-unlist(strsplit(vars, splc[i], fixed =TRUE)) 
-  }
-  vars<-gsub(" ", "", vars, fixed=TRUE)
-  vars<-vars[!is.element(toupper(vars), c("", "FACTOR","AS.FACTOR","STRATA"))]
+  spec.str<-c("FACTOR","AS.FACTOR","STRATA")
+  vars<-unlist(strsplit(model, "[,() ~]+"))
+  vars<-vars[!is.element(toupper(vars), spec.str)] 
   if (sum(!is.element(vars, dimnames(clin)[[2]]))!=0)
-     {print("Model Specification Error:"); break()} 
-  var2<-unlist(strsplit(modd, "~"))
+     {print("Model Specification Error:"); break()}
+      
+  var2<-unlist(strsplit(model, "~"))
   var2<-gsub(" ", "", var2, fixed=TRUE)
-  var2<-var2[!is.element(var2, '')]
-  
-  #Define new model form to include an extra term for projected data
-  if (length(var2)==2 ) newmod<-paste(var2[1], " ~ prb + ", var2[2], sep="")
-  if (length(var2)==1 &length(vars)>1) newmod<-paste(var2[1], " ~ prb", sep="")
-  if (length(var2)==1 &length(vars)==1) newmod<-"ph ~ prb"
+  if (var2[2]!="" ) newmodel<-paste(var2[1], "~ prb + ", var2[2], sep="")
+  if (var2[2]=="" &length(vars)>1) newmodel<-paste(var2[1], "~ prb", sep="")
+  if (var2[2]=="" &length(vars)==1) newmodel<-"ph ~ prb"
+
   nsub<-nrow(clin)
   res<-NULL
    
   set.seed(seed)
   perms<-matrix(NA, nboots, nsub)
-  for (i in 1:nboots) perms[i,]<-sample(nsub,replace=TRUE)
-  
-  for (i in 1:length(geneSet))
+  for (i in seq_len(nboots)) perms[i,]<-sample(nsub,replace=TRUE)
+   
+  nSet<-length(geneSet)  
+  for (i in seq_len(nSet))
   {
     thisSet<-geneSet[[i]]
     this<-geneIds(thisSet)
@@ -52,68 +47,62 @@ POSTglm<-function(exprSet,
        val<-egd$values
        vec<-egd$vectors
        if (is.null(lamda)) nf<-length(val)
-          else if (is.numeric(lamda)) 
-               nf<-min(c(1:length(val))[cumsum(val)/sum(val) >=lamda])
+       if (is.numeric(lamda)) 
+             nf<-min(seq_len(length(val))[cumsum(val)/sum(val) >=lamda])
        if (nf >1)
        {          
-         AA<-diag(val[1:nf])
-         prj.pot<-t(t(this.pot)%*%vec[, 1:nf])
+         AA<-diag(val[seq_len(nf)])
+         prj.pot<-t(t(this.pot)%*%vec[, seq_len(nf)])
          zstat<-apply(prj.pot, 1, function(prb, mdat){
               if (length(vars)==1)  tdd<-cbind.data.frame(prb, ph= mdat)
                 else tdd<-cbind.data.frame(prb, mdat)             
-              #tres<-summary(glm(as.formula(newmod), family=family, data=tdd,...))
-              tres<-summary(glm(as.formula(newmod), family=family, data=tdd))
+              tres<-summary(glm(as.formula(newmodel), family=family, data=tdd,...))
               return(tres$coefficients[2, 3])
               }, clin[, vars])
          qstat<-t(zstat)%*%AA%*%zstat
          
          # bootstrap to estimate the correlation structure, under null.     
-         boots<-NULL
-         for (j in 1:nboots)
+         boots<-matrix(NA, nboots, nf)
+         for (j in seq_len(nboots))
          {
             this.boot<-perms[j,]
-            boot.t<-apply(prj.pot, 1, function(prb, mdat){
+            boot.res<-apply(prj.pot, 1, function(prb, mdat){
               if (length(vars)==1)  tdd<-cbind.data.frame(prb, ph= mdat)
                   else tdd<-cbind.data.frame(prb, mdat)
-              tres<-summary(glm(as.formula(newmod), family=family, data=tdd,...))
+              tres<-summary(glm(as.formula(newmodel), family=family, data=tdd,...))
               return(tres$coefficients[2, 3])
               }, clin[this.boot, vars])
-          boots<-rbind(boots, boot.t)
+            boots[j, ]<-boot.res
          }
          sigma<-cor(boots)
          Asigma<-AA%*%sigma
          npdlam<-nearPD(Asigma)$eigenvalues
-         lambda<-npdlam[npdlam>0] 
-         #Asigma.decomp<-eigen(Asigma)
-         #lambda<-Asigma.decomp$values[Asigma.decomp$values>0]           
+         lambda<-npdlam[npdlam>0]           
          gchiappr<-davies(qstat, lambda)$Qq
          this.res<-cbind(Gene=setName(thisSet), Nprobe=nrow(this.pot),  
                          Ndim=nf, Stat=qstat, Pvalue=gchiappr)
          res<-rbind(res, this.res)
-         #print(paste(i, nf, sep=": "))
        }
       if (nf ==1)
       {
         tdd<-cbind.data.frame(prb=t(this.pot)%*%vec[, 1], clin[, vars])
-        tres<-summary(glm(as.formula(newmod), family=family, data=tdd,...))
+        tres<-summary(glm(as.formula(newmodel), family=family, data=tdd,...))
         zstat<-tres$coefficients[2, 3]^2
         pval<-pchisq(zstat, 1, lower.tail=FALSE)
         this.res<-cbind(Gene=setName(thisSet),Nprobe=nrow(this.pot), 
                         Ndim=1,  Stat=zstat, Pvalue=pval)
         res<-rbind(res, this.res) 
-        #print(paste(i, nf, sep=": "))
       }    
     }    
     if (nrow(this.pot)==1)
     {
         tdd<-cbind.data.frame(prb=as.numeric(this.pot), clin[, vars])
-        tres<-summary(glm(as.formula(newmod), family=family, data=tdd,...))
+        tres<-summary(glm(as.formula(newmodel), family=family, data=tdd,...))
         zstat<-tres$coefficients[2, 3]^2
         pval<-pchisq(zstat, 1, lower.tail=FALSE)
         this.res<-cbind(Gene=setName(thisSet),Nprobe=nrow(this.pot), 
                         Ndim=1,  Stat=zstat, Pvalue=pval)
         res<-rbind(res, this.res)
-        #print(paste(i, 1, sep=": "))
     }
   }
   dimnames(res)[[2]]<-c("GeneSet", "Nprobe", "Nproj", 'Stat', 'p.value')
